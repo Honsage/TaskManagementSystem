2961ba57ca540af50c0bb819f87613ca93ad23d2
Honsage <musatov.i.a@edu.mirea.ru>
2026-08-28 13:57:56 +0300

    feat(domain): specify validation annotations in model

--- src/main/java/ru/honsage/practice/taskmanagementsystem/domain/Task.java
+++ src/main/java/ru/honsage/practice/taskmanagementsystem/domain/Task.java
@@ -1,15 +1,22 @@
 package ru.honsage.practice.taskmanagementsystem.domain;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record Task (
         Long id,
+        @NotNull
         Long creatorId,
         Long assignedUserId,
         TaskStatus status,
         LocalDateTime createDateTime,
+        @NotNull
+        @Future
         LocalDate deadlineDate,
+        @NotNull
         TaskPriority priority
 ) {
 }
